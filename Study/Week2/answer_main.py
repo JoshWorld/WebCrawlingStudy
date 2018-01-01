207d85a7369a45b669a74b0b3f39b0f2ca336110
TaegyeongLee <luckperson7@naver.com>
2018-01-01 15:58:27 +0900

    get comment

--- Study/Week2/answer_main.py
+++ Study/Week2/answer_main.py
@@ -27,14 +27,15 @@ def keyword_search(keyword, page_count):
             if news_link.text == "네이버뉴스":
                 naver_news_links.append(news_link['href'])
                 i = i + 1
-                print(naver_news_links)
                 if i == int(page_count):
                     return naver_news_links
         j = j * 10
 
 
-def get_comments(news_link):
+def get_comments(news_link, user_comment_count):
+
     comment_count = 0
+    comment_list = []
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
@@ -44,16 +45,29 @@ def get_comments(news_link):
     aid = news_link.split("aid=")[1]
     oid = news_link.split("oid=")[1].split("&")[0]
 
-    r = requests.get(
-        "https://apis.naver.com/commentBox/cbox/web_neo_list_jsonp.json?ticket=news&templateId=default_politics&pool=cbox5&_callback=jQuery17023240944630416482_1506390886908&lang=ko&country=&objectId=news" + oid + "%2C" + aid + "&categoryId=&pageSize=20&indexSize=10&groupId=&listType=OBJECT&page=1&sort=FAVORITE&current=1079250985&prev=1079229065&includeAllStatus=true&_=1506390900990",
-        headers=headers)
-    c = r.content
-    soup = BeautifulSoup(c, "html.parser")
-    contents = str(soup).split('"contents":"')
-    for content in contents:
-        comment_content = content.split('","userIdNo"')[0]
-        comment_count = comment_count + 1
-        print(comment_content)
+    page = 1
+    while True:
+        r = requests.get(
+            "https://apis.naver.com/commentBox/cbox/web_neo_list_jsonp.json?ticket=news&templateId=default_politics&pool=cbox5&_callback=jQuery17023240944630416482_1506390886908&lang=ko&country=&objectId=news" + oid + "%2C" + aid + "&categoryId=&pageSize=20&indexSize=10&groupId=&listType=OBJECT&page=" + str(page) + "&sort=FAVORITE&current=1079250985&prev=1079229065&includeAllStatus=true&_=1506390900990",
+            headers=headers)
+        c = r.content
+        soup = BeautifulSoup(c, "html.parser")
+
+        c_count = int(int(str(soup).split('{"comment":')[1].split(",")[0])/20)
+
+        contents = str(soup).split('"contents":"')
+        for i in range(0, len(contents)):
+            comment_content = contents[i].split('","userIdNo"')[0]
+            comment_count = comment_count + 1
+
+            comment_list.append(comment_content)
+            if user_comment_count == comment_count:
+                return comment_list
+
+        if c_count < 1 or c_count == page:
+            break
+        else:
+            page = page + 1
 
 
 if __name__ == '__main__':
@@ -62,4 +76,4 @@ if __name__ == '__main__':
     comments = input("크롤링해올 덧글 갯수를 입력해주세요 : ")
     news_links = keyword_search(keyword,news)
     for news_link in news_links:
-        get_comments(news_link)
+        get_comments(news_link, comments)
