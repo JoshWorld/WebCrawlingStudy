4d25feea4dd161301bad94d7803251901038035b
TaeGyeongLee <taegyeong7202@gmail.com>
2018-07-03 11:18:58 +0900

    README

--- Season02/Selenium/code01.py
+++ Season02/Selenium/code01.py
@@ -1,12 +1,13 @@
+
 from selenium import webdriver
 
-browser = webdriver.Chrome("/Users/itaegyeong/Desktop/webcrawling/Season02/Selenium/chromedriver")
-browser.get("https://www.instagram.com/accounts/login/")
+browser = webdriver.Chrome("/Users/itaegyeong/Desktop/2018spring/webcrawling/Season02/Selenium/chromedriver")
+browser.get("http://naver.com")
 
-id_text = browser.find_element_by_name("username")
-pw_text = browser.find_element_by_name("password")
+id_text = browser.find_element_by_css_selector('#id')
+pw_text = browser.find_element_by_css_selector('#pw')
 
-button = browser.find_element_by_tag_name("button")
+button = browser.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/span/input')
 
 id_text.send_keys("test_id")
 pw_text.send_keys("testpassword")
